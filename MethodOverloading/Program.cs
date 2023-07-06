a9d69e91a286d5cbcb54a1a410507a5e07d38158
felix adnel <flixadnel@gmail.com>
2023-07-06 01:56:30 -0500

    done

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,49 @@
 ﻿namespace MethodOverloading
 {
-    public class Program
+    class Program
     {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool isTrue)
+        {
+            var sum = Add(a, b);
+            var response = "";
+
+            if (isTrue)
+            {
+                sum = a + b;
+
+                if (sum == 1)
+                {
+                    return $"{sum} dollar";
+                }
+                else
+                {
+                    return $"{sum} dollars";
+                }
+            
+            }
+
+            return response;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(0, 1, true));
+            Console.WriteLine();
+            Console.WriteLine(Add(5, 7,true));  
+            Console.WriteLine();
+            Console.WriteLine(Add(-6, 3, true));
+
         }
     }
 }
+
